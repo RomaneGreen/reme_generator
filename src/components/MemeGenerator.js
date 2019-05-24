d70051a417162f13f50eafab9a2448b58f0f5634
Romane G <romane71193@gmail.com>
2019-05-24 14:25:44 -0400

    cdm functional

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class MemeGenerator extends Component {
+
+    componentDidMount() {
+        alert("halo!")
+    }
+
+  render() {
+
+    
+    
+    
+    
+    return (
+      <div>
+        <p>Hey i extend baby</p>
+      </div>
+    )
+  }
+}
